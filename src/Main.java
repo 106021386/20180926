cef655d5ef29a2e2d6e6dc3d23f742af6ed6b133
USER <106021386@live.asia.edu.tw>
2018-09-26 15:44:29 +0800

    123

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,25 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scn = new Scanner (System.in);
-        int h =scn.nextInt();
-        int t =scn.nextInt();
-        if(t == 1){
-            System.out.println((h-80)*0.7f );
+        Scanner scn = new Scanner(System.in);
+        int a =scn.nextInt(),
+            b =scn.nextInt(),
+            c =scn.nextInt(),
+            r =scn.nextInt();
+        int f = r-(a*15+b*20+c*30);
+
+        if (f<0){
+            System.out.println("0");
         }
         else {
-            System.out.println((h-70)*0.6f);
+            System.out.println(f/50);
+            f = f%50;
+            System.out.println(f/5);
+            System.out.println(f%5);
+
+
+
+
         }
-
     }
-
-
-
 }
